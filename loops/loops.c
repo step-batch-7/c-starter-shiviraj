c20efc5734cfccff9ffc5431e903a82878917e83
Shivam Rajput <58025059+shiviraj@users.noreply.github.com>
2020-03-28 16:30:24 +0530

    Added function print_fibonacci

--- loops/loops.c
+++ loops/loops.c
@@ -5,12 +5,24 @@ int find_factorial(int num)
   return num ? num * find_factorial(num - 1) : 1;
 }
 
+void print_fibonacci(int num)
+{
+  int current_term = 0;
+  int next_term = 1;
+  while (num > 0)
+  {
+    printf("%d\n", current_term);
+    next_term = current_term + next_term;
+    current_term = next_term - current_term;
+    num--;
+  }
+}
+
 int main(void)
 {
   int num;
   printf("Enter a num: ");
   scanf("%d", &num);
-  int factorial = find_factorial(num);
-  printf("%d\n", factorial);
+  print_fibonacci(num);
   return 0;
 }

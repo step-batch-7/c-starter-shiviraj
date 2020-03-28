78ccfc6397820daea98536284d29d06db0e14e80
Shivam Rajput <58025059+shiviraj@users.noreply.github.com>
2020-03-28 14:47:52 +0530

    Added find_gcd function

--- basic_functions.c
+++ basic_functions.c
@@ -15,12 +15,17 @@ int find_square(int num)
   return num * num;
 }
 
+int find_cube(int num)
+{
+  return num * find_square(num);
+}
+
 int main(void)
 {
   int num;
   printf("Enter a number: ");
   scanf("%d", &num);
-  char square = find_square(num);
+  char square = find_cube(num);
   printf("%d\n", square);
   return 0;
 }

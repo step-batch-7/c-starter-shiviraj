1b9d136be46465ceec40fd1ebb09acd47abf40af
Shivam Rajput <58025059+shiviraj@users.noreply.github.com>
2020-03-28 16:44:16 +0530

    Added print_even_between_two function
    Added print_every_2nd_num

--- loops/loops.c
+++ loops/loops.c
@@ -23,13 +23,24 @@ unsigned char is_odd(int num)
   return num % 2;
 }
 
-void print_odd_between_two(int start_num, int end_num)
+void print_every_2nd_num(int start_num, int end_num)
 {
-  start_num = is_odd(start_num) ? start_num : start_num++;
   for (int num = start_num; num <= end_num; num += 2)
   {
     printf("%d\n", num);
   }
+}
+
+void print_odd_between_two(int start_num, int end_num)
+{
+  start_num = is_odd(start_num) ? start_num : ++start_num;
+  print_every_2nd_num(start_num, end_num);
+}
+
+void print_even_between_two(int start_num, int end_num)
+{
+  start_num = is_odd(start_num) ? ++start_num : start_num;
+  print_every_2nd_num(start_num, end_num);
 }
 
 int main(void)
@@ -37,6 +48,6 @@ int main(void)
   int num;
   printf("Enter a num: ");
   scanf("%d", &num);
-  print_odd_between_two(1, num);
+  print_even_between_two(1, num);
   return 0;
 }

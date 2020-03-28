b1e66df8adfe1647630c63d9546c0459386389a3
Shivam Rajput <58025059+shiviraj@users.noreply.github.com>
2020-03-28 16:38:48 +0530

    Added print_odd_num_between_two function

--- loops/loops.c
+++ loops/loops.c
@@ -18,11 +18,25 @@ void print_fibonacci(int num)
   }
 }
 
+unsigned char is_odd(int num)
+{
+  return num % 2;
+}
+
+void print_odd_between_two(int start_num, int end_num)
+{
+  start_num = is_odd(start_num) ? start_num : start_num++;
+  for (int num = start_num; num <= end_num; num += 2)
+  {
+    printf("%d\n", num);
+  }
+}
+
 int main(void)
 {
   int num;
   printf("Enter a num: ");
   scanf("%d", &num);
-  print_fibonacci(num);
+  print_odd_between_two(1, num);
   return 0;
 }
